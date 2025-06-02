56c05082ff798fd9b8f237f760770749205508bc
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-06-02 18:54:21 +0700

    menampilkan public

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -6,3 +6,12 @@ class remoteLampu
 private:
      string SaklarNo[10];
 
+public:
+    void setSaklarNo(int i, string value)
+    {
+        SaklarNo[i] = value;
+    }
+    string getSaklarNo(int i)
+    {
+        return SaklarNo[i];
+    }
