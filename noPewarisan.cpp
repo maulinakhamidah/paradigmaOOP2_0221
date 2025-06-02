92547c3afb56d8ee22558465e11a25bee7f8a77a
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-06-02 19:26:35 +0700

    last commit

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 using namespace std;
 
+class baseClass 
+{
+public:
+    virtual void perkenalan() final
+    {
+        cout << "Hallo saya Funtion dari base class";
+    }
+};
 
+class derivedClass : public baseClass
+{
+public:
+    void perkenalan()
+    {
+        cout << "Hallo saya Funtion dari derived Class";
+    }
+};
+
+int main()
+{
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
+}
 
 
 
