679fb41e0d2b739df58e7b852fe849a1e6eff363
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-06-02 19:12:35 +0700

    memanggil method

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -15,3 +15,21 @@ public:
     {
         return SaklarNo[i];
     }
+};
+
+int main()
+{
+    remoteLampu lampuRumah;
+
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(2, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(3, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(4, "Lampu Dapur");
+
+    cout << lampuRumah.getSaklarNo(0) << endl;
+    cout << lampuRumah.getSaklarNo(1) << endl;
+    cout << lampuRumah.getSaklarNo(2) << endl;
+    cout << lampuRumah.getSaklarNo(3) << endl;
+
+    return 0;
+}
